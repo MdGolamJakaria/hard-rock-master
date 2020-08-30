028e0a29ccefd7d02485dd8e97800758109eed56
Jakaria-P-Hero <jakaria3335@gmail.com>
2020-08-30 11:13:42 +0600

    inimum Requirement Done

--- main.js
+++ main.js
@@ -7,21 +7,36 @@ document.getElementById('search-btn').addEventListener('click',function(){
         .then ( data =>{
             // console.log(data.data.id)
             data= data.data.slice(0,10);
+
+            const listArea = document.getElementById('show-area');
+            listArea.innerHTML='';
             for (let i = 0; i < data.length; i++) {
                 const songs = data[i];
-                console.log(songs)
-                const listArea = document.getElementById('show-area')
+                // console.log(songs)
+                // searchBar = '';
+                
                 const p = document.createElement('p');
-                p.innerHTML= `
-                    <p class="author lead"> 
-                        <strong> ${songs.title}</strong> 
-                        Album by 
-                        <span> ${songs.artist.name} </span> 
-                        <button class="btn btn-success"> Get Lyrics </button> 
-                    </p> `
+                p.innerHTML= ` 
+                    <div class="single-result row align-items-center my-3 p-3">
+                    <div class="col-md-9">
+                        <h3 class="lyrics-name">${songs.title}</h3>
+                        <p class="author lead">Album by <span>${songs.artist.name}</span></p>
+                    </div>
+                    <div class="col-md-3 text-md-right text-center">
+                        <button class="btn btn-success">Get Lyrics</button>
+                    </div>
+                    </div>
+                     `
                     listArea.appendChild(p);
+        
             }
+            
         })
+
+        
+
+        
+        
 })
 
 
